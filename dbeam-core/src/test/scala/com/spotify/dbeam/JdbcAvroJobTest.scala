9c19b05a269aedfadb4203287f2c11c995973668
Luis Bianchin <labianchin@users.noreply.github.com>
2018-05-14 08:16:33 +0200

    Refactor tests

--- dbeam-core/src/test/scala/com/spotify/dbeam/JdbcAvroJobTest.scala
+++ dbeam-core/src/test/scala/com/spotify/dbeam/JdbcAvroJobTest.scala
@@ -21,16 +21,15 @@ import java.io.File
 import java.util
 import java.util.UUID
 
-import com.spotify.scio.testing._
 import org.apache.avro.Schema
 import org.apache.avro.generic.GenericRecord
 import org.apache.beam.sdk.io.AvroSource
 import org.apache.beam.sdk.testing.SourceTestUtils
 import org.apache.commons.io.FileUtils
-import slick.jdbc.H2Profile.api._
 import org.scalatest._
+import slick.jdbc.H2Profile.api._
 
-class JdbcAvroJobTest extends PipelineSpec with Matchers with BeforeAndAfterAll {
+class JdbcAvroJobTest extends FlatSpec with Matchers with BeforeAndAfterAll {
   private val connectionUrl: String =
     "jdbc:h2:mem:test2;MODE=PostgreSQL;DATABASE_TO_UPPER=false;DB_CLOSE_DELAY=-1"
   private val db: Database = Database.forURL(connectionUrl, driver = "org.h2.Driver")
@@ -47,8 +46,7 @@ class JdbcAvroJobTest extends PipelineSpec with Matchers with BeforeAndAfterAll 
   override protected def afterAll(): Unit = FileUtils.deleteDirectory(dir)
 
   "JdbcAvroJob" should "work" in {
-    JobTest[JdbcAvroJob.type]
-      .args(
+    JdbcAvroJob.main(Array(
         "--partition=2025-02-28",
         "--skipPartitionCheck",
         "--connectionUrl=" + connectionUrl,
@@ -56,15 +54,14 @@ class JdbcAvroJobTest extends PipelineSpec with Matchers with BeforeAndAfterAll 
         "--password=",
         "--table=coffees",
         "--output=" + dir.getAbsolutePath)
-//        .output[String](TextIO(dir.getAbsolutePath + "/_queries"))(_ should haveSize(1))
-      .run()
+    )
     val files: Array[File] = dir.listFiles()
-    files.map(_.getName) should contain theSameElementsAs (Seq(
+    files.map(_.getName) should contain theSameElementsAs Seq(
       "_AVRO_SCHEMA.avsc", "_METRICS.json", "_SERVICE_METRICS.json",
-      "_queries", "part-00000-of-00001.avro"))
+      "_queries", "part-00000-of-00001.avro")
     files.filter(_.getName.equals("_queries"))(0).listFiles().map(_.getName) should
-      contain theSameElementsAs (Seq("query_0.sql"))
-    val schema = (new Schema.Parser()).parse(new File(dir, "_AVRO_SCHEMA.avsc"))
+      contain theSameElementsAs Seq("query_0.sql")
+    val schema = new Schema.Parser().parse(new File(dir, "_AVRO_SCHEMA.avsc"))
     val source: AvroSource[GenericRecord] = AvroSource
       .from(new File(dir, "part-00000-of-00001.avro").toString)
       .withSchema(schema)
