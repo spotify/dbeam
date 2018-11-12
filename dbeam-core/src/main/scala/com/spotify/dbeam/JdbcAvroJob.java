4e919de23e8c2e5f29e7bb55ab03d79aa74564bb
Luis Bianchin <labianchin@users.noreply.github.com>
2018-11-12 07:50:27 +0100

    main classes in java

---
+++ dbeam-core/src/main/scala/com/spotify/dbeam/JdbcAvroJob.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2018 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.spotify.dbeam;
+
+import com.google.common.base.Preconditions;
+
+import com.spotify.dbeam.options.JdbcExportArgs;
+import com.spotify.dbeam.options.JdbcExportArgsFactory;
+import com.spotify.dbeam.options.OptionsParser;
+
+import org.apache.avro.Schema;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.PipelineResult;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.transforms.Create;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+public class JdbcAvroJob {
+
+  private static Logger LOGGER = LoggerFactory.getLogger(JdbcAvroJob.class);
+
+  private final Pipeline pipeline;
+  private final JdbcExportArgs jdbcExportArgs;
+  private final String output;
+  private final PipelineOptions pipelineOptions;
+
+  public JdbcAvroJob(PipelineOptions pipelineOptions) throws IOException, ClassNotFoundException {
+    this.pipelineOptions = pipelineOptions;
+    this.pipeline = Pipeline.create(pipelineOptions);
+    this.jdbcExportArgs = JdbcExportArgsFactory.fromPipelineOptions(pipelineOptions);
+    this.output =  OptionsParser.getOutput(pipelineOptions);
+    Preconditions.checkArgument(this.output != null && this.output.length()>0, "'output' must be defined");
+  }
+
+  public void prepareExport() throws IOException {
+    final Schema generatedSchema = BeamJdbcAvroSchema.createSchema(this.pipeline, jdbcExportArgs);
+    BeamHelper.saveStringOnSubPath(output, "/_AVRO_SCHEMA.avsc", generatedSchema.toString(true));
+    final List<String> queries = StreamSupport.stream(
+        jdbcExportArgs.queryBuilderArgs().buildQueries().spliterator(), false).collect(Collectors.toList());
+
+    for (int i = 0; i < queries.size(); i++) {
+      BeamHelper.saveStringOnSubPath(output, String.format("/_queries/query_%d.sql", i),
+                                     queries.get(i));
+    }
+    LOGGER.info("Running queries: {}", queries.toString());
+
+    pipeline.apply("JdbcQueries", Create.of(queries))
+        .apply("JdbcAvroSave", JdbcAvroIO.Write.createWrite(
+            output,
+            ".avro",
+            generatedSchema,
+            jdbcExportArgs.jdbcAvroOptions()
+        ));
+  }
+
+  public Pipeline getPipeline() {
+    return pipeline;
+  }
+
+  public JdbcExportArgs getJdbcExportArgs() {
+    return jdbcExportArgs;
+  }
+
+  public String getOutput() {
+    return output;
+  }
+
+  public PipelineOptions getPipelineOptions() {
+    return pipelineOptions;
+  }
+
+  public PipelineResult runAndWait() {
+    return BeamHelper.waitUntilDone(this.pipeline.run());
+  }
+
+  public PipelineResult runExport() throws IOException {
+    prepareExport();
+    final PipelineResult pipelineResult = runAndWait();
+    BeamHelper.saveMetrics(MetricsHelper.getMetrics(pipelineResult), output);
+    return pipelineResult;
+  }
+
+  public static void main(String[] cmdLineArgs) throws IOException, ClassNotFoundException {
+    final PipelineOptions pipelineOptions = OptionsParser.buildPipelineOptions(cmdLineArgs);
+    new JdbcAvroJob(pipelineOptions).runExport();
+  }
+
+}
---
+++ dbeam-core/src/main/scala/com/spotify/dbeam/PsqlAvroJob.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2018 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.spotify.dbeam;
+
+import com.google.api.client.util.Preconditions;
+
+import com.spotify.dbeam.options.JdbcExportArgs;
+import com.spotify.dbeam.options.OptionsParser;
+
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.joda.time.DateTime;
+import org.joda.time.Duration;
+import org.joda.time.ReadablePeriod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PsqlAvroJob extends JdbcAvroJob {
+  private static final Logger LOGGER = LoggerFactory.getLogger(PsqlAvroJob.class);
+  private static final String REPLICATION_QUERY =
+      "SELECT now() AS current_timestamp, " +
+      "pg_last_xact_replay_timestamp() AS last_replication, " +
+      "ROUND (( EXTRACT (EPOCH FROM now()) - " +
+      "EXTRACT (EPOCH FROM pg_last_xact_replay_timestamp()) " +
+      ") * 1000) AS replication_delay;";
+
+  public PsqlAvroJob(PipelineOptions pipelineOptions)
+      throws IOException, ClassNotFoundException {
+    super(pipelineOptions);
+    validateOptions(this.getJdbcExportArgs());
+  }
+
+  static void validateOptions(JdbcExportArgs jdbcExportArgs) {
+    Preconditions.checkArgument(
+        jdbcExportArgs.jdbcAvroOptions().jdbcConnectionConfiguration()
+            .driverClassName().contains("postgres"),
+        "Must be a PostgreSQL connection");
+    Preconditions.checkArgument(
+        jdbcExportArgs.queryBuilderArgs().partition().isPresent(),
+        "Partition parameter must be defined");
+  }
+
+  boolean isReplicationDelayed() {
+    final DateTime partition = this.getJdbcExportArgs().queryBuilderArgs().partition().get();
+    final DateTime lastReplication = queryReplication();
+    final ReadablePeriod partitionPeriod =
+        this.getJdbcExportArgs().queryBuilderArgs().partitionPeriod();
+    return isReplicationDelayed(partition, lastReplication, partitionPeriod);
+  }
+
+  static boolean isReplicationDelayed(DateTime partition, DateTime lastReplication,
+                                              ReadablePeriod partitionPeriod) {
+    if (lastReplication.isBefore(partition.plus(partitionPeriod))) {
+      LOGGER.error("Replication was not completed for partition, " +
+                   "expected >= {}, actual = {}",
+                   partition.plus(partitionPeriod), lastReplication);
+      return true;
+    }
+    return false;
+  }
+
+  static DateTime queryReplication(Connection connection, String query) throws SQLException {
+    final ResultSet resultSet = connection.createStatement().executeQuery(query);
+    Preconditions.checkState(resultSet.next(), "Replication query returned empty results");
+    DateTime lastReplication = new DateTime(resultSet.getTimestamp("last_replication"));
+    Duration replicationDelay = new Duration(resultSet.getLong("replication_delay"));
+    LOGGER.info("Psql replication check lastReplication={} replicationDelay={}",
+                lastReplication, replicationDelay);
+    return lastReplication;
+  }
+
+  private DateTime queryReplication() {
+    LOGGER.info("Checking PostgreSQL replication lag...");
+    try (Connection connection = this.getJdbcExportArgs().createConnection()) {
+      return queryReplication(connection, REPLICATION_QUERY);
+    } catch (Exception e) {
+      throw new IllegalStateException("Could not fetch replication lag", e);
+    }
+  }
+
+  public static void main(String[] cmdLineArgs) throws IOException, ClassNotFoundException {
+    final PipelineOptions pipelineOptions = OptionsParser.buildPipelineOptions(cmdLineArgs);
+    PsqlAvroJob job = new PsqlAvroJob(pipelineOptions);
+
+    if (job.isReplicationDelayed()) {
+      System.exit(20);
+    } else {
+      job.runExport();
+    }
+  }
+
+}
