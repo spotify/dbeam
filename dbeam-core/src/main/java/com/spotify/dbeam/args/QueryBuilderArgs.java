f873a01f1cbf81e51443526d3ca1deaad4ea75f3
Luis Bianchin <labianchin@users.noreply.github.com>
2021-05-11 12:11:45 +0200

    update license

--- dbeam-core/src/main/java/com/spotify/dbeam/args/QueryBuilderArgs.java
+++ dbeam-core/src/main/java/com/spotify/dbeam/args/QueryBuilderArgs.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -108,9 +108,7 @@ public abstract class QueryBuilderArgs implements Serializable {
     return createBuilder().setBaseSqlQuery(QueryBuilder.fromSqlQuery(sqlQuery)).build();
   }
 
-  /**
-   * Returns query with limit one, so it can be used to query and fetch schema.
-   */
+  /** Returns query with limit one, so it can be used to query and fetch schema. */
   public String sqlQueryWithLimitOne() {
     return this.baseSqlQuery().withLimit(1L).build();
   }
--- dbeam-core/src/main/java/com/spotify/dbeam/avro/JdbcAvroRecordConverter.java
+++ dbeam-core/src/main/java/com/spotify/dbeam/avro/JdbcAvroRecordConverter.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -56,8 +56,8 @@ public class JdbcAvroRecordConverter {
     final int columnCount = meta.getColumnCount();
 
     final JdbcAvroRecord.SqlFunction<ResultSet, Object>[] mappings =
-      (JdbcAvroRecord.SqlFunction<ResultSet, Object>[])
-        new JdbcAvroRecord.SqlFunction<?, ?>[columnCount + 1];
+        (JdbcAvroRecord.SqlFunction<ResultSet, Object>[])
+            new JdbcAvroRecord.SqlFunction<?, ?>[columnCount + 1];
 
     for (int i = 1; i <= columnCount; i++) {
       mappings[i] = JdbcAvroRecord.computeMapping(meta, i);
